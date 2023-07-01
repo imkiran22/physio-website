f7c0ac96af58f7794d827fd8ab2c374b0574a287
imkiran22 <kiran22292@gmail.com>
2023-07-01 16:53:01 +0530

    changes

--- pages/book-appointment.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import BookAppointment from "../components/BookAppointment/BookAppointment";
-
-const BookPage = () => {
-  return <BookAppointment/>
-}
-
-export default BookPage;
